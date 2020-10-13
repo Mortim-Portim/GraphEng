e10034f40f20837f5c24d026de17c8bb81a349ad
Mortim-Portim <marvin@lapdell>
2020-10-13 18:36:31 +0200

    bliblablub

--- Compression/universal_compression.go
+++ Compression/universal_compression.go
@@ -1,27 +1,21 @@
 package Compression
 
 import (
-	"math/big"
+	
 )
 
-
-func CompressAll(changing [][]byte, static ...[]byte) (comp []byte) {
-	comp = make([]byte, 0)
+func Merge(changing [][]byte, static ...[]byte) (merged []byte) {
+	merged = make([]byte, 0)
 	for _,bs := range(static) {
-		comp = append(comp, bs...)
+		merged = append(merged, bs...)
 	}
 	for _,bs := range(changing) {
-		i := &big.Int{}; i.SetInt64(int64(len(bs)))
-		comp = append(comp, BigIntToBytes(i)...)
-		comp = append(comp, bs...)
+		merged = append(merged, Int64ToBytes(int64(len(bs)))...)
+		merged = append(merged, bs...)
 	}
-	comp,_ = CompressBytes(comp)
 	return
 }
-
-//static 0-n, changing 0-n
-func DecompressAll(comp []byte, length []int) (BS [][]byte) {
-	comp,_ = DecompressBytes(comp)
+func Demerge(comp []byte, length []int) (BS [][]byte) {
 	BS = make([][]byte, 0)
 	currIdx := 0
 	for _,l := range(length) {
@@ -30,13 +24,24 @@ func DecompressAll(comp []byte, length []int) (BS [][]byte) {
 	}
 	
 	for true {
-		length := BytesToBigInt(comp[currIdx:currIdx+8])
+		length := int(BytesToInt64(comp[currIdx:currIdx+8]))
 		currIdx += 8
-		BS = append(BS, comp[currIdx:currIdx+int(length.Int64())])
-		currIdx += int(length.Int64())
+		BS = append(BS, comp[currIdx:currIdx+length])
+		currIdx += length
 		if currIdx >= len(comp) {
 			break
 		}
 	}
+	return
+}
+func CompressAll(changing [][]byte, static ...[]byte) (comp []byte) {
+	comp,_ = CompressBytes(Merge(changing, static...))
+	return
+}
+
+//static 0-n, changing 0-n
+func DecompressAll(comp []byte, length []int) (BS [][]byte) {
+	comp,_ = DecompressBytes(comp)
+	BS = Demerge(comp, length)
 	return
 }

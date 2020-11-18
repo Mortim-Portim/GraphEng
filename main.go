c8437c08167d2c3d74a3851c48c2f9bf2545f704
BuildTools <unconfigured@null.spigotmc.org>
2020-11-18 17:22:25 +0100

    Update file system

--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
+	"github.com/mortim-portim/GraphEng/GE"
+	"github.com/mortim-portim/TN_Engine/TNE"
+
 	"github.com/hajimehoshi/ebiten"
-	"marvin/GraphEng/GE"
-	"marvin/TN_Engine/TNE"
-	
-	"marvin/GraphEng/res"
+
 	"fmt"
 	"time"
+
+	"github.com/mortim-portim/GraphEng/res"
+
 	"github.com/hajimehoshi/ebiten/ebitenutil"
-	
 	//"image/color"
-	
 	//cmp "marvin/GraphEng/Compression"
 )
+
 const (
 	screenWidth  = 1600
 	screenHeight = 900
-	FPS = 30
-	TestText = "Licht ist eine Form der elektromagnetischen Strahlung. Im engeren Sinne sind vom gesamten elektromagnetischen Spektrum nur die Anteile gemeint, die für das menschliche Auge sichtbar sind. Im weiteren Sinne werden auch elektromagnetische Wellen kürzerer Wellenlänge (Ultraviolett) und größerer Wellenlänge (Infrarot) dazu gezählt.\nDie physikalischen Eigenschaften des Lichts werden durch verschiedene Modelle beschrieben: In der Strahlenoptik wird die geradlinige Ausbreitung des Lichts durch „Lichtstrahlen“ veranschaulicht; in der Wellenoptik wird die Wellennatur des Lichts betont, wodurch auch Beugungs- und Interferenzerscheinungen erklärt werden können. In der Quantenphysik schließlich wird das Licht als ein Strom von Quantenobjekten, den Photonen (veranschaulichend auch „Lichtteilchen“ genannt), beschrieben. Eine vollständige Beschreibung des Lichts bietet die Quantenelektrodynamik. Im Vakuum breitet sich Licht mit der konstanten Lichtgeschwindigkeit von 299.792.458 m/s aus. Trifft Licht auf Materie, so kann es gestreut, reflektiert, gebrochen und verlangsamt oder absorbiert werden.\nLicht ist der für das menschliche Auge adäquate Sinnesreiz. Dabei wird die Intensität des Lichts als Helligkeit wahrgenommen, die spektrale Zusammensetzung als Farbe."
+	FPS          = 30
+	TestText     = "Licht ist eine Form der elektromagnetischen Strahlung. Im engeren Sinne sind vom gesamten elektromagnetischen Spektrum nur die Anteile gemeint, die für das menschliche Auge sichtbar sind. Im weiteren Sinne werden auch elektromagnetische Wellen kürzerer Wellenlänge (Ultraviolett) und größerer Wellenlänge (Infrarot) dazu gezählt.\nDie physikalischen Eigenschaften des Lichts werden durch verschiedene Modelle beschrieben: In der Strahlenoptik wird die geradlinige Ausbreitung des Lichts durch „Lichtstrahlen“ veranschaulicht; in der Wellenoptik wird die Wellennatur des Lichts betont, wodurch auch Beugungs- und Interferenzerscheinungen erklärt werden können. In der Quantenphysik schließlich wird das Licht als ein Strom von Quantenobjekten, den Photonen (veranschaulichend auch „Lichtteilchen“ genannt), beschrieben. Eine vollständige Beschreibung des Lichts bietet die Quantenelektrodynamik. Im Vakuum breitet sich Licht mit der konstanten Lichtgeschwindigkeit von 299.792.458 m/s aus. Trifft Licht auf Materie, so kann es gestreut, reflektiert, gebrochen und verlangsamt oder absorbiert werden.\nLicht ist der für das menschliche Auge adäquate Sinnesreiz. Dabei wird die Intensität des Lichts als Helligkeit wahrgenommen, die spektrale Zusammensetzung als Farbe."
 )
+
 func StartGame(g ebiten.Game) {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("GraphEng Test")
@@ -39,13 +42,12 @@ func StartGame(g ebiten.Game) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 /**
-//  _    _                 _____       _             __                
-// | |  | |               |_   _|     | |           / _|               
-// | |  | |___  ___ _ __    | |  _ __ | |_ ___ _ __| |_ __ _  ___ ___  
-// | |  | / __|/ _ \ '__|   | | | '_ \| __/ _ \ '__|  _/ _` |/ __/ _ \ 
-// | |__| \__ \  __/ |     _| |_| | | | ||  __/ |  | || (_| | (_|  __/ 
+//  _    _                 _____       _             __
+// | |  | |               |_   _|     | |           / _|
+// | |  | |___  ___ _ __    | |  _ __ | |_ ___ _ __| |_ __ _  ___ ___
+// | |  | / __|/ _ \ '__|   | | | '_ \| __/ _ \ '__|  _/ _` |/ __/ _ \
+// | |__| \__ \  __/ |     _| |_| | | | ||  __/ |  | || (_| | (_|  __/
 //  \____/|___/\___|_|    |_____|_| |_|\__\___|_|  |_| \__,_|\___\___|
 
 
@@ -103,7 +105,7 @@ func main() {
 	//----------------------------------------------------------------------------------------------------------------------------------------------
 	//Creates a ScrollBar with min=0, max=120, initvalue=3
 	scrollBar := GE.GetStandardScrollbar(700, 500, 600, 60, 0, 120, 3, GE.StandardFont)
-	
+
 	//----------------------------------------------------------------------------------------------------------------------------------------------
 	//Loads an ebiten image from bytes
 	spellEimg,_ := GE.LoadEbitenImgFromBytes(res.SPELL_ANIM)
@@ -131,10 +133,10 @@ func main() {
 	outerTabViewUpdateAble[1] = button
 	outerTabViewUpdateAble[2] = textView
 	outerTabViewUpdateAble[3] = tabView_inner
-	
+
 	params_outer := &GE.TabViewParams{Nms: []string{"Fett", "Sack", "Fettsack", "LOL"}, Scrs: outerTabViewUpdateAble, W: screenWidth, H: screenHeight}
 	tabView_outer := GE.GetTabView(params_outer)
-	
+
 	//----------------------------------------------------------------------------------------------------------------------------------------------
 	g := &TestGame{tabView_outer, nil, nil, 0}
 
@@ -142,39 +144,38 @@ func main() {
 }
 **/
 
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-// __          __        _     _    _____ _                   _                  
-// \ \        / /       | |   | |  / ____| |                 | |                 
-//  \ \  /\  / /__  _ __| | __| | | (___ | |_ _ __ _   _  ___| |_ _   _ _ __ ___ 
+// __          __        _     _    _____ _                   _
+// \ \        / /       | |   | |  / ____| |                 | |
+//  \ \  /\  / /__  _ __| | __| | | (___ | |_ _ __ _   _  ___| |_ _   _ _ __ ___
 //   \ \/  \/ / _ \| '__| |/ _` |  \___ \| __| '__| | | |/ __| __| | | | '__/ _ \
 //    \  /\  / (_) | |  | | (_| |  ____) | |_| |  | |_| | (__| |_| |_| | | |  __/
 //     \/  \/ \___/|_|  |_|\__,_| |_____/ \__|_|   \__,_|\___|\__|\__,_|_|  \___|
 
-
 type TestGame struct {
-	wrld *GE.WorldStructure
-	rec  *GE.Recorder
+	wrld   *GE.WorldStructure
+	rec    *GE.Recorder
 	player *TNE.Entity
-	frame int
+	frame  int
 }
+
 func (g *TestGame) Init(screen *ebiten.Image) {
 	g.wrld.Add_Drawables = g.wrld.Add_Drawables.Add(g.player)
 }
 
 var timeTaken int64
 var inputT, objT, lightUpT, lightDT, worldDT time.Duration
+
 func (g *TestGame) Update(screen *ebiten.Image) error {
 	startTime := time.Now()
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		x,y := ebiten.CursorPosition()
-		xt,yt := g.wrld.GetTileOfCoords(x,y)
-		fmt.Printf("You clicked at (%v|%v) tile (%v|%v)\n", x,y, xt,yt)
+		x, y := ebiten.CursorPosition()
+		xt, yt := g.wrld.GetTileOfCoords(x, y)
+		fmt.Printf("You clicked at (%v|%v) tile (%v|%v)\n", x, y, xt, yt)
 	}
 	/**
 	g.player.SetIsMoving(false)
@@ -201,40 +202,40 @@ func (g *TestGame) Update(screen *ebiten.Image) error {
 	if dy != 0 {
 		g.wrld.Lights[0].SetMaximumIntesity(g.wrld.Lights[0].GetMaximumIntesity()+int16(dy*10))
 	}
-	
+
 	x,y := g.wrld.SmoothMiddle()
 	g.player.Update(nil)
 	g.player.SetTo(float64(x),float64(y))
-	
+
 	//oX, oY,_ := g.player.GetPos()
 	//g.player.SetPosMD(oX,oY)
-	
+
 	g.player.UpdateOrientation()
 	g.wrld.UpdateObjDrawables()
 	**/
-	
+
 	if g.frame%100 < 50 {
-		for _,strct := range(g.wrld.Structures) {
+		for _, strct := range g.wrld.Structures {
 			strct.IsUnderstood = true
 		}
-	}else{
-		for _,strct := range(g.wrld.Structures) {
+	} else {
+		for _, strct := range g.wrld.Structures {
 			strct.IsUnderstood = false
 		}
 	}
-	
+
 	g.wrld.UpdateLightLevel(1)
 	u_lights := g.wrld.UpdateAllLightsIfNecassary()
-	
+
 	//Around 8ms
 	g.wrld.Draw(screen)
-	
+
 	if ebiten.IsKeyPressed(ebiten.KeyC) && !g.rec.IsSaving() {
 		g.rec.Save("./res/out")
 	}
 	g.rec.NextFrame(screen)
-	
-	g.frame ++
+
+	g.frame++
 	timeTaken = time.Now().Sub(startTime).Milliseconds()
 	fps := ebiten.CurrentTPS()
 	msg := fmt.Sprintf(`TPS: %0.2f, Updating took: %v at frame %v, ul:%v`, fps, timeTaken, g.frame-1, u_lights)
@@ -252,33 +253,33 @@ func main() {
 	GE.StandardFont = GE.ParseFontFromBytes(res.MONO_TTF)
 	GE.SetLogFile("./res/log.txt")
 	time.Sleep(time.Second)
-	
-	pathFMat := GE.GetMatrix(10,10,0)
+
+	pathFMat := GE.GetMatrix(10, 10, 0)
 	for x := 2; x < 7; x++ {
-		pathFMat.Set(x,3,10)
+		pathFMat.Set(x, 3, 10)
 	}
-	pathFMat.Set(2,4,10)
-	pathFMat.Set(2,5,10)
+	pathFMat.Set(2, 4, 10)
+	pathFMat.Set(2, 5, 10)
 	fmt.Println(pathFMat.Print())
-	
+
 	nanos := 0
 	for i := 0; i < 100; i++ {
 		stComp := time.Now()
-		GE.FindPathMat(pathFMat, [2]int{1,1}, [2]int{8,8}, true)
+		GE.FindPathMat(pathFMat, [2]int{1, 1}, [2]int{8, 8}, true)
 		nanos += int(time.Now().Sub(stComp).Nanoseconds())
 	}
 	fmt.Println("Computing A* took: ", time.Duration(nanos/100))
-	
-	FP := GE.FindPathMat(pathFMat, [2]int{1,1}, [2]int{8,8}, false)
-	for _,p := range(FP) {
-		pathFMat.Set(p[0],p[1],888)
+
+	FP := GE.FindPathMat(pathFMat, [2]int{1, 1}, [2]int{8, 8}, false)
+	for _, p := range FP {
+		pathFMat.Set(p[0], p[1], 888)
 		fmt.Println(p)
 	}
 	fmt.Println(pathFMat.Print())
-	
-	XT := 50; YT := 50
-	
-	
+
+	XT := 50
+	YT := 50
+
 	//----------------------------------------------------------------------------------------------------------------------------------------------
 	//Creates the index Matrix
 	wmatI := GE.GetMatrix(300, 300, 0)
@@ -297,44 +298,48 @@ func main() {
 	//----------------------------------------------------------------------------------------------------------------------------------------------
 	//Creates a WorldStructure object
 	wrld := GE.GetWorldStructure(0, 0, screenWidth, screenHeight, XT, YT, 32, 18)
-	
+
 	//----------------------------------------------------------------------------------------------------------------------------------------------
 	errtl := wrld.LoadTiles("./res/tiles/")
-	if errtl != nil {panic(errtl)}
+	if errtl != nil {
+		panic(errtl)
+	}
 	errol := wrld.LoadStructureObjs("./res/structObjs/")
-	if errol != nil {panic(errol)}
-	
-	wrld.AddNamedStructureObj("house1", 	10, 2)
-	wrld.AddNamedStructureObj("tree2", 		5, 5)
-	wrld.AddNamedStructureObj("tree2big", 	14, 14)
-	
+	if errol != nil {
+		panic(errol)
+	}
+
+	wrld.AddNamedStructureObj("house1", 10, 2)
+	wrld.AddNamedStructureObj("tree2", 5, 5)
+	wrld.AddNamedStructureObj("tree2big", 14, 14)
+
 	wrld.UpdateObjMat()
-	
+
 	obs := wrld.ObjectsToBytes()
 	fmt.Printf("Objects are %v bytes\n", len(obs))
 	wrld.BytesToObjects(obs)
-	
+
 	//player,err := GE.GetWObjFromPath("./res/anims/test")
 	//if err != nil {panic(err)}
-	
+
 	//----------------------------------------------------------------------------------------------------------------------------------------------
 	//Add a light source to the world
-	light1 := GE.GetLightSource(&GE.Point{10,10}, &GE.Vector{0,-1,0}, 360, 400, 0.01, false)
-	light2 := GE.GetLightSource(&GE.Point{15,15}, &GE.Vector{0,-1,0}, 360, 400, 0.01, false)
-	
-	wrld.Lights = append(wrld.Lights, light1, light2)//, light2, light3, light4, light5, light6, light7, light8, light9, light10)
+	light1 := GE.GetLightSource(&GE.Point{10, 10}, &GE.Vector{0, -1, 0}, 360, 400, 0.01, false)
+	light2 := GE.GetLightSource(&GE.Point{15, 15}, &GE.Vector{0, -1, 0}, 360, 400, 0.01, false)
+
+	wrld.Lights = append(wrld.Lights, light1, light2) //, light2, light3, light4, light5, light6, light7, light8, light9, light10)
 	wrld.UpdateLIdxMat()
 	wrld.UpdateLightValue(wrld.Lights, true)
-	
+
 	lbs := wrld.LightsToBytes()
 	fmt.Printf("Lights are %v bytes\n", len(lbs))
 	wrld.BytesToLights(lbs)
-	
+
 	//Sets the start point
-	wrld.SetMiddle(14,14,true)
-	wrld.SetLightStats(10,255, 0.3)
+	wrld.SetMiddle(14, 14, true)
+	wrld.SetLightStats(10, 255, 0.3)
 	wrld.SetLightLevel(15)
-	
+
 	//----------------------------------------------------------------------------------------------------------------------------------------------
 	//Saves the compressed world
 	startComp := time.Now()
@@ -345,43 +350,42 @@ func main() {
 	//Calculates how long it took to save the world
 	endComp := time.Now()
 	fmt.Println("Saving wrld took: ", endComp.Sub(startComp))
-	
+
 	//loads the compressed world
 	startDeComp := time.Now()
-	newWrld, errL := GE.LoadWorldStructure(0,0,screenWidth,screenHeight, "./res/TestMap4.map", "./res/tiles/", "./res/structObjs/")
+	newWrld, errL := GE.LoadWorldStructure(0, 0, screenWidth, screenHeight, "./res/TestMap4.map", "./res/tiles/", "./res/structObjs/")
 	if errL != nil {
 		GE.ShitImDying(errL)
 	}
 	//Calculates how long it took to load the world
 	endDeComp := time.Now()
 	fmt.Println("Loading wrld took: ", endDeComp.Sub(startDeComp))
-	
+
 	//Sets the start point
 	//newWrld.SetMiddle(14,14,true)
-	newWrld.SetLightStats(10,255, 0.3)
+	newWrld.SetLightStats(10, 255, 0.3)
 	newWrld.SetLightLevel(15)
 	//Creates a raster
 	//newWrld.GetFrame(2, 90)
-	newWrld.SetDisplayWH(32,18)
-	
-	g := &TestGame{newWrld, GE.GetNewRecorder(FPS*5, 1280, 720, FPS), nil, 0}	
-	
+	newWrld.SetDisplayWH(32, 18)
+
+	g := &TestGame{newWrld, GE.GetNewRecorder(FPS*5, 1280, 720, FPS), nil, 0}
+
 	cf, err := TNE.GetEntityFactory("./res/creatures/", &g.frame, 3)
 	GE.ShitImDying(err)
-	
+
 	prepStart := time.Now()
 	cf.Prepare()
 	fmt.Println("Preparing took: ", time.Now().Sub(prepStart))
-	
+
 	getStart := time.Now()
 	c := cf.Get(0)
 	fmt.Println("Getting took: ", time.Now().Sub(getStart))
 	g.player = c
-	
+
 	g.Init(nil)
 	StartGame(g)
 }
-
 
 /**
 type TestGame struct {}
@@ -392,25 +396,25 @@ func (g *TestGame) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	GE.Init("")
-	
+
 	go func(){
 		st, err := GE.LoadSoundTrack("./res/audio/Soundtrack")
 		GE.ShitImDying(err)
 		st.Play("main")
 		fmt.Println("Playing Main")
-		
+
 		time.Sleep(time.Second*3)
 		st.Play("ork")
 		fmt.Println("Playing Ork")
-		
+
 		time.Sleep(time.Second*3)
 		st.Pause()
 		fmt.Println("Pausing")
-		
+
 		time.Sleep(time.Second*3)
 		st.Resume()
 		fmt.Println("Resuming")
-		
+
 		time.Sleep(time.Second*3)
 		st.FadeOut()
 		fmt.Println("Fading out")

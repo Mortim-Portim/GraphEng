429ccf76733bada5ddde2b3ce2b9a0cdaa19ca06
Mortim-Portim <>
2021-02-26 17:26:51 +0100

    fett

--- GE/particle.go
+++ GE/particle.go
@@ -1,15 +1,29 @@
 package GE
 
+func GetTimedParticle() {
+
+}
+
 type Particle struct {
 	*WObj
 	Mass     float32
 	Velocity *Vector
 	Forces   []*Force
+
+	TimedOut      bool
+	TimedOutFrame int
 }
 
-func (p *Particle) Update() {
+func (p *Particle) Update(frame int) {
+	if frame >= p.TimedOutFrame {
+		p.TimedOut = true
+	}
+	p.WObj.Update(frame)
 	for _, f := range p.Forces {
 		p.Velocity.Add(f.GetAcc(float64(p.Mass)))
 	}
 	p.MoveBy(p.Velocity.X, p.Velocity.Y)
+}
+func (p *Particle) IsFinished() bool {
+	return p.TimedOut
 }

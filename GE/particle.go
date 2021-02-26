5182eabb9b23cdb0c967e6eaee91342788cfdbc6
Mortim-Portim <>
2021-02-26 17:08:31 +0100

    fett

--- GE/particle.go
+++ GE/particle.go
@@ -2,6 +2,14 @@ package GE
 
 type Particle struct {
 	*WObj
-	Mass   float64
-	Forces []*Force
+	Mass     float32
+	Velocity *Vector
+	Forces   []*Force
+}
+
+func (p *Particle) Update() {
+	for _, f := range p.Forces {
+		p.Velocity.Add(f.GetAcc(float64(p.Mass)))
+	}
+	p.MoveBy(p.Velocity.X, p.Velocity.Y)
 }
